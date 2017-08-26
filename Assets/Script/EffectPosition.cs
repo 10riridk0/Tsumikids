2876572d8168904b25ed6939f7000eaa2f0733b0
Juri <10ririmi0kd@gmail.com>
2017-08-26 16:36:54 +0900

    ・GameObjectにEffectPositionアタッチした ・スクリプトからスケールを変更できるようにした

--- Assets/Script/EffectPosition.cs
+++ Assets/Script/EffectPosition.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EffectPosition : MonoBehaviour {
-	void Start ()
+
+    public ParticleSystemScalingMode scalingMode;
+    void Start ()
     {
         //this.transform.position = new Vector3(10, 10, 10);
 
-        //Resourcesフォルダの中の「Stretch 03 PS(プレハブ)」を取得する
-        GameObject prefab = (GameObject)Resources.Load("EffectPrefab/Stretch 03 PS");
+        GameObject prefab = (GameObject)Resources.Load("EffectPrefab/Stretch 03 PS");   //Resourcesフォルダの中の「Stretch 03 PS(プレハブ)」を取得する
+        ParticleSystem ps = prefab.GetComponent<ParticleSystem>();                      //パーティクルシステムのコンポーネントを取得
         //プレハブからインスタンス生成
         //Instantiate( 生成するオブジェクト,  場所, 回転): 回転はそのままなら" Quaternion.identity"らしい
-        Instantiate(prefab, new Vector3(1, 1, 1), Quaternion.identity);
+        Instantiate(prefab, new Vector3(10, 10, 10), Quaternion.identity);
+
+        scalingMode = ParticleSystemScalingMode.Hierarchy;
+        transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        
     }
 
 	void Update () {
