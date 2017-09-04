ebd85949db3439ce34a656a0e25625f1c5e75505
Juri <10ririmi0kd@gmail.com>
2017-09-04 15:11:45 +0900

    クリックしたら音が鳴るPlaySound.csを追加しました。また、BGM・効果音を入れるSoundフォルダを置いてあります。

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+    AudioSource audioSource;
+    //public AudioClip BGM;
+    public AudioClip SE;
+
+	// Use this for initialization
+	void Start () {
+        //AudioSourceの動的生成, コンポーネント取得
+        gameObject.AddComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //クリックしたら効果音を流す
+        if (Input.GetMouseButtonDown(0))
+        {
+            //第1引数: AudioClipの名前
+            //第２引数: 大きさ
+            audioSource.PlayOneShot(SE, 0.7f);
+        }
+	}
+}
