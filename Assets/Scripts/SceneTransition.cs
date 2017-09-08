1a3db8052a8226bce4f1030bfdf081ef91e38419
Juri <10ririmi0kd@gmail.com>
2017-09-08 23:52:31 +0900

    FadeCanvasが2つ生成されないように、フェードインし終えたらfadeCanvasを削除するようにした

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -6,31 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour {
 
-    GameObject fadePrefab, scenePrefab;
-    public static GameObject fadeCanvas;
-    public static GameObject sceneTransition;
+    GameObject fadePrefab, scenePrefab;                         //プレハブを取得する
+    public static GameObject fadeCanvas;                        //フェードするためのCanvas
+    public static GameObject sceneTransition;                   //シーン遷移するためのGameObject
     public static Image image;
-    public static float r, b, g, alfa;        //alfa: 1のとき不透明, 0のとき透明
-    public static float fadeSpeed = 0.07f;
-    public static bool isFadeOut = false, isFadeIn = false;
+    public static float r, b, g, alfa;                          //alfa: 1のとき不透明, 0のとき透明
+    public static float fadeSpeed = 0.07f;                      //フェードスピード
+    public static bool isFadeOut = false, isFadeIn = false;     //フェードしたか否かを判定する変数
 
     //フェードイン関数
+    //isFadeInがfalseの間フェードインし続ける
     public static void FadeIn()
     {
         if (!isFadeIn)
         {
             Debug.Log("FadeIn alfa>> " + alfa);
             alfa -= fadeSpeed;
-            image.color = new Color(r, g, b, alfa);
+            image.color = new Color(r, g, b, alfa);     //透明度を更新
             if (alfa < 0)
             {
                 Debug.Log("フェードイン終了");
-                isFadeIn = true;
-                fadeCanvas.SetActive(false);
+                isFadeIn = true;                        //alfa < 0なのでフェードイン終了
+                Destroy(fadeCanvas);                    //必要ないのでfadeCanvasを削除
             }
         }
     }
     //フェードアウト関数
+    //isFadeOutがfalseの間フェードアウトし続ける
     public static void FadeOut()
     {
         fadeCanvas.SetActive(true);
@@ -59,8 +61,8 @@ public class SceneTransition : MonoBehaviour {
             fadePrefab = (GameObject)Resources.Load("FadePrefab/FadeCanvas");
             fadeCanvas = (GameObject)Instantiate(fadePrefab);
         }
-        DontDestroyOnLoad(fadeCanvas);                                          //シーン遷移しても破棄しないようにする(永続的に保持)
         DontDestroyOnLoad(sceneTransition);
+        DontDestroyOnLoad(fadeCanvas);                                          //シーン遷移しても破棄しないようにする(永続的に保持)
         image = GameObject.Find("Panel").GetComponent<Image>();                 //PanelのImageコンポーネント取得
         fadeCanvas.SetActive(false);                                            //非表示にする
     }
@@ -70,15 +72,18 @@ public class SceneTransition : MonoBehaviour {
     //仮引数: 遷移したいシーンの名前
     public static void ChangeScene(string sceneName)
     {
+        Debug.Log("フェードアウト開始");
         FadeOut();
         if (isFadeOut)                               //フェードアウトし終えたら次のシーンを読み込む
         {
-            SceneManager.LoadScene(sceneName);
+            SceneManager.LoadScene(sceneName);       //シーン読み込み
             Debug.Log("シーン遷移終了");
         }
     }
+    //フェードイン関数
     public static void SceneFadeIn()
     {
+        Debug.Log("フェードイン開始");
         FadeIn();
     }
 }
