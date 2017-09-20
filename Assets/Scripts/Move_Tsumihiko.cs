38e6e9573b009632665ce4c714daf3b5cc92ec77
rinpanman <rinpanman06@gmail.com>
2017-09-20 15:02:58 +0900

    つみひこの移動する関数作った！！！

---
+++ Assets/Scripts/Move_Tsumihiko.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Tsumihiko : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("move", 'd');
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    //つみひこ移動関数
+    IEnumerator move(char direction)
+    {
+        Transform trans_tsumihiko = GetComponent<Transform>();
+        Vector3 before_change = trans_tsumihiko.position;
+        Vector3 add = new Vector3(0, 0, 0);
+        add = direction_dicied(add, direction);
+        Debug.Log(add);
+
+        while (true)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                yield return new WaitForSeconds(0.1f); // ３秒待つ
+                
+                trans_tsumihiko.position += add;
+                Debug.Log(trans_tsumihiko.position);
+            }
+
+            trans_tsumihiko.position = before_change;
+        }
+    }
+
+    Vector3 direction_dicied(Vector3 add, char direction)
+    {
+        switch (direction)
+        {
+            case 'u':
+                add.y = 10;
+                break;
+            case 'd':
+                add.y = -10;
+                break;
+            case 'r':
+                add.x = 10;
+                break;
+            case 'l':
+                add.x = -10;
+                break;
+        }
+
+        return (add);
+    }
+}
---
+++ Assets/Scripts/Stage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour {
+
+    static int[,] start_position = new int[,] {{-585, -296}, {-585, -296}, {-735, -446}};
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
