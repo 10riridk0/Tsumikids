59ec3239da8f97ee72f58dc7893559373bb07c88
rinpanman <rinpanman06@gmail.com>
2017-10-03 16:25:08 +0900

    mainシーンからStage1に移動するようにした！(失った進捗の一部)

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Menu : MonoBehaviour {
+public class MainMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        SaveData.Clear();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        SceneTransition.ChangeScene("Stage1");
 	}
 }
